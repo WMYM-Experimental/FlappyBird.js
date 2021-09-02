5d4c2ed1e7d1212efd02e4283dc24a7cc4b68493
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-02 15:59:59 -0500

    made a bird variable

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innnerWidth;
-canvas.height = window.innnerHeight;
+//canvas.width = window.innnerWidth;
+//canvas.height = window.innnerHeight;
+
+let bird = new Image();
 
 function getRandomInt(min, max) {
   let randomInt = Math.floor(Math.random() * (max - min + 1) + min);
