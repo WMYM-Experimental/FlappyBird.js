6c7c22383d7e05020c91da304729d887cb179413
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-02 16:21:13 -0500

    added the respective initial variables for the game

--- js/script.js
+++ js/script.js
@@ -2,11 +2,37 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-//canvas.width = window.innnerWidth;
-//canvas.height = window.innnerHeight;
+//game images
+let bird = new Image(); //user
+let background = new Image();
+let floor = new Image();
+let pipeBootom = new Image();
+let pipeTop = new Image();
 
-let bird = new Image();
+let scoreSound = new Audio(); //sound effect
 
+//game variables
+let distanceBetweenPipes = 80;
+let gravity = 1;
+
+//bird variables
+let birdLocationX = 10;
+let birdLocationY = 150;
+
+//score
+let score = 0;
+
+//relative paths images
+bird.src = "img/game_imgs/bird.png";
+background.src = "img/game_imgs/background.png";
+floor.src = "img/game_imgs/floor.png";
+pipeBootom.src = "img/game_imgs/pipeBottom";
+pipeTop.src = "img/game_imgs/pipeTop.png";
+
+//relative paths sounds
+scoreSound.src = "sounds/score.mp3";
+
+//bird jump
 function getRandomInt(min, max) {
   let randomInt = Math.floor(Math.random() * (max - min + 1) + min);
 
